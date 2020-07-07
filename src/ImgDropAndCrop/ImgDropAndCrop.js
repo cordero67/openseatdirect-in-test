3fabad4bf16ec09b27f6cc6ba12b9f4e77c28c4f
cordero67 <rafael.cordero@ymail.com>
2020-07-07 15:47:11 -0400

    ImgDropAndCrop

--- src/ImgDropAndCrop/ImgDropAndCrop.js
+++ src/ImgDropAndCrop/ImgDropAndCrop.js
@@ -10,10 +10,13 @@ import Aux from "../hoc/Auxiliary/Auxiliary";
 
 import classes from "./Backdrop.module.css";
 
-import {base64StringtoFile,
-    downloadBase64File,
-    extractImageFileExtensionFromBase64,
-    image64toCanvasRef,image64toCanvasRef2} from './ResuableUtils'
+//import {base64StringtoFile,
+//    downloadBase64File,
+ //   extractImageFileExtensionFromBase64,
+//    image64toCanvasRef,image64toCanvasRef2} from './ResuableUtils'
+
+
+import {extractImageFileExtensionFromBase64,image64toCanvasRef4,image64toCanvasRef2,image64toCanvasRef3, imagetoNaturalCanvas} from './ResuableUtils';
 
 import { Button } from 'semantic-ui-react';
 
@@ -21,17 +24,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage } from "@fortawesome/free-solid-svg-icons";
 
 const imageMaxSize = 1000000 // bytes
-const acceptedFileTypes = 'image/x-png, image/png, image/jpg, image/jpeg, image/gif'
+//const acceptedFileTypes = 'image/x-png, image/png, image/jpg, image/jpeg, image/gif'
+
+// see https://github.com/jshttp/mime-db
+// types from: http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types
+
+// 'image/bmp, image/cgm, image/g3fax,image/gif,image/ief,image/jpeg,image/ktx,image/png,
+// image/prs.btif, image/sgi, image/svg+xml, image/tiff,image/vnd.adobe.photoshop,
+// mime type                                    extension
+//  image/gif                                        gif;
+//  image/jpeg                                       jpeg jpg;
+//  image/png                                        png;
+//  image/svg+xml                                    svg svgz;
+//  image/tiff                                       tif tiff;
+//  image/vnd.wap.wbmp                               wbmp;
+//  image/webp                                       webp;
+const acceptedFileTypes = 'image/gif, image/jpeg, image/png, image/svg+xml, image/webp';
 const acceptedFileTypesArray = acceptedFileTypes.split(",").map((item) => {return item.trim()})
 
 class ImgDropAndCrop extends Component {
     constructor(props){
         super(props);
-        //console.log("this.props: ", this.props);
+        console.log("this.props: ", this.props);
         //console.log("props.photoData: ", props.info);
         this.imagePreviewCanvasRef = React.createRef();
         this.fileInputRef = React.createRef();
-        //console.log("init:this.imagePreviewCanvasRef.current:", this.imagePreviewCanvasRef.current);
+        console.log("init:this.imagePreviewCanvasRef.current:", this.imagePreviewCanvasRef.current);
         this.state = {
             imgSrc: null,
             imgSrcExt: null,
@@ -54,28 +72,31 @@ class ImgDropAndCrop extends Component {
                 height: null
             }
         }
-        //console.log("state ending in constructor: ", this.state)
+        console.log("state ending in constructor: ", this.state)
     }
     
     IMAGE_WIDTH = 0;
     IMAGE_HEIGHT = 0;
 
     verifyFile = (files) => {
-        console.log (files);
+        console.log ("files>> ", files);
         if (files && files.length > 0){
             const currentFile = files[0];
-            if (!currentFile.file){
-                alert(" Only images are allowed.")
+            console.log ("files[0]>> ", currentFile);
+            if (!currentFile){
+                alert(" Only image files are allowed.")
                 return false               
             };
-            const currentFileType = currentFile.file.type;
-            const currentFileSize = currentFile.file.size;
+            const currentFileType = currentFile.type;
+            const currentFileSize = currentFile.size;
             if(currentFileSize > imageMaxSize) {
-                alert("This file is not allowed. File size cannot exceed " + imageMaxSize + " bytes ");
+                let showmax = (imageMaxSize/1000).toLocaleString('en');
+                alert("This file is not allowed. File size cannot exceed " + showmax + " Kbytes ");
                 return false
             }
             if (!acceptedFileTypesArray.includes(currentFileType)){
-                alert("This file is not allowed. Only images are allowed.")
+                let showmax = (imageMaxSize/1000).toLocaleString('en');
+                alert("File must be an image file with size no greater than " + showmax + " Kbytes ");
                 return false
             }
             return true
@@ -83,8 +104,10 @@ class ImgDropAndCrop extends Component {
     }
 
     handleOnDrop = (files, rejectedFiles) => {
-        //console.log (files); // MM
-        //console.log ("rejected files are ", rejectedFiles); // MM
+        console.log ("in handleOnDrop");
+        console.log ("files = ", files); // MM
+        console.log ("rejected =", rejectedFiles); // MM
+
         if (rejectedFiles && rejectedFiles.length > 0){
             this.verifyFile(rejectedFiles)
         }
@@ -92,17 +115,18 @@ class ImgDropAndCrop extends Component {
         if (files && files.length > 0){
             const isVerified = this.verifyFile(files)
             if (isVerified){
-                const currentFile = files[0]
-                const myFileItemReader = new FileReader()
+                const currentFile = files[0];
+                const myFileItemReader = new FileReader();
                 myFileItemReader.addEventListener("load", ()=>{
-                    const myResult = myFileItemReader.result
+                    const myResult = myFileItemReader.result;
                     this.setState({
                         imgSrc: myResult,
                         imgSrcExt: extractImageFileExtensionFromBase64(myResult),
                         isCropping: true
                     })
-                }, false)
-                myFileItemReader.readAsDataURL(currentFile)
+                }, false);
+                // added listener now reading file
+                myFileItemReader.readAsDataURL(currentFile);
             }
         }
     }
@@ -110,25 +134,25 @@ class ImgDropAndCrop extends Component {
     defaultCrop = (image) => {
         // sets the default crop. Centered and maximized space subject aspect ratio of 2
         const ASPECT_RATIO = 2/1;
-        //console.log("**defaultCrop");
+        console.log("**defaultCrop");
         const w  = image.width;
         const h  = image.height;
         let xa,ya,wa,ha;
         // this is for a 2 by 1 crop
         if (w > ASPECT_RATIO * h) { // extra wide image
-            //console.log ("exta wide..");
+            console.log ("exta wide..");
             ya = 0;
             xa = (w - (ASPECT_RATIO*h))/2;
             wa = ASPECT_RATIO*h;
             ha = h;
         } else {             // narrow image
-            //console.log ("exta narrow..");
+            console.log ("exta narrow..");
             xa = 0;
             ya = (h - (w/ASPECT_RATIO))/2;
             wa = w;
             ha = w/ASPECT_RATIO;
         };
-        //console.log ("default crop: w h xa ya wa ha ", w, h, xa, ya, wa, ha);
+        console.log ("default crop: w h xa ya wa ha ", w, h, xa, ya, wa, ha);
         let {crop}= this.state;
         crop.aspect=ASPECT_RATIO;
         crop.ruleOfThirds= true;
@@ -137,14 +161,14 @@ class ImgDropAndCrop extends Component {
         crop.width=wa ;
         crop.height = ha;
         this.setState({crop:crop})
-        //console.log ("setting default crop:", crop);
+        console.log ("setting default crop:", crop);
 
     }
 
     handleImageLoaded = (image) => {
-        //console.log("**handleimageLoaded", image);
+        console.log("**handleimageLoaded", image);
         let{percentCrop} = this.state;
-        //console.log("handleimageLoaded percentCrop: ",percentCrop)        
+        console.log("handleimageLoaded percentCrop: ",percentCrop)        
         if (!(
             percentCrop.x === null ||
             percentCrop.y === null ||
@@ -153,7 +177,7 @@ class ImgDropAndCrop extends Component {
         )) {
             const w  = image.width;
             const h  = image.height;
-            //console.log("inside handleImageLoaded  image width, height = ", w,h);
+            console.log("inside handleImageLoaded  image width, height = ", w,h);
 
             let {crop} = this.state;
             crop.aspect = 2;
@@ -177,34 +201,40 @@ class ImgDropAndCrop extends Component {
     }
 
     handleOnCropChange = (crop) => {
-        //console.log("**handleOnCropChange", crop)
-        //console.log ("this.imagePreviewCanvasRef:", this.imagePreviewCanvasRef);
-        //console.log ("this.state:", this.state);
+        console.log("**handleOnCropChange",crop);
+//        console.log ("this.imagePreviewCanvasRef:", this.imagePreviewCanvasRef);
+//        console.log ("this.state:", this.state);
         //this.props.change(crop);
         this.setState({crop:crop})
     }
 
     handleOnCropComplete = (crop, percentCrop) =>{
-        //console.log("handleOnCropCompleted crop, percentCrop:", crop, percentCrop);
+        console.log("handleOnCropCompleted crop, percentCrop:", crop, percentCrop);
         this.setState({percentCrop: percentCrop});
         const canvasRef = this.imagePreviewCanvasRef.current;
         const {imgSrc}  = this.state;
         image64toCanvasRef2(canvasRef, imgSrc, percentCrop);
-        //console.log("Before imagePreviewCanvasRef: ",this.imagePreviewCanvasRef);
-        //console.log ("magePreviewCanvasRef.current.width and height" ,this.imagePreviewCanvasRef.current.width,this.imagePreviewCanvasRef.current.height)
+  //      console.log("Before imagePreviewCanvasRef: ",this.imagePreviewCanvasRef);
+  //      console.log ("magePreviewCanvasRef.current.width and height" ,this.imagePreviewCanvasRef.current.width,this.imagePreviewCanvasRef.current.height)
     }
 
 
     handleCreateCroppedImage = async (event) => {
-        //console.log ("in handleCreateCroppedImage..");
+        console.log ("in handleCreateCroppedImage..");
         event.preventDefault()
-        const {imgSrc} = this.state;
+        const {imgSrc,percentCrop} = this.state;
         const canvasRef = this.imagePreviewCanvasRef.current;        
-        //console.log(">>>canvasRef, imgSrc",canvasRef,imgSrc);
+//        console.log(">>>canvasRef, imgSrc",canvasRef,imgSrc);
         if (canvasRef && imgSrc) { 
             const {imgSrcExt} =  this.state;
-            const tempImage = canvasRef.toDataURL('image/' +imgSrcExt, 0.92);
+            await image64toCanvasRef2(canvasRef, imgSrc, percentCrop);
+//            const tempImage = canvasRef.toDataURL('image/' +imgSrcExt);
+            const tempImage = canvasRef.toDataURL('image/png');
+
+//            const imageBlob = await new Promise((resolve) => canvasRef.toBlob(resolve, "image/png"));
             const imageBlob = await new Promise((resolve) => canvasRef.toBlob(resolve, "image/png"));
+//            this.setState({newimageData64: tempImage});  
+//            const tempImage = URL.createObjectURL(imageBlob);
             this.setState({newimageData64: tempImage});  
             this.props.change(imageBlob);  // sends imageBlob to parent using change prop
         }
@@ -213,15 +243,11 @@ class ImgDropAndCrop extends Component {
     }
 
     handleClearToDefault = event =>{
-        //console.log ("handleClearToDefault", event);
+        console.log ("handleClearToDefault", event);
 
         if (event) event.preventDefault();
-
-
         const canvas = this.imagePreviewCanvasRef.current;
-
         if ((!this.state.newimageData64) && canvas) {
-
             const ctx = canvas.getContext('2d');
             ctx.clearRect(0, 0, canvas.width, canvas.height)
 
@@ -246,7 +272,7 @@ class ImgDropAndCrop extends Component {
     }
 
     newClear = event =>{
-        //console.log ("newClear", event);
+        console.log ("newClear", event);
         if (event) event.preventDefault()
 
         this.setState({
@@ -271,7 +297,7 @@ class ImgDropAndCrop extends Component {
     }
 
     handleFileSelect = event => {
-        //console.log ("handleFileSelect", event);
+        console.log ("handleFileSelect", event);
         const files = event.target.files
         if (files && files.length > 0){
             const isVerified = this.verifyFile(files)
@@ -294,9 +320,9 @@ class ImgDropAndCrop extends Component {
 
     static getDerivedStateFromProps(nextProps, prevState){
         //lifecycle function to update child state with props set by parent
-        //console.log ("in getderivedStateFromProps > nextprop: ", nextProps);
-        //console.log ("in getderivedStateFromProps > prevProps:  ", prevState);
-        //console.log ("nextProps.imagein!==prevState.imgSrc  :", nextProps.imagein!==prevState.imgSrc);
+        console.log ("in getderivedStateFromProps > nextprop: ", nextProps);
+        console.log ("in getderivedStateFromProps > prevProps:  ", prevState);
+        console.log ("nextProps.imagein!==prevState.imgSrc  :", nextProps.imagein!==prevState.imgSrc);
         if (prevState.imgSrcLoaded){ // disable getDerivedStateFromProps after image is loaded once. 
             return null;            // we don't need this function in EventCreation, we need it in EventEdit
         }
@@ -305,7 +331,7 @@ class ImgDropAndCrop extends Component {
             return null;
         };
         if((nextProps.imagein.isLoaded!==prevState.imgSrcLoaded) || nextProps.imagein.imgSrc!==prevState.imgSrc){
-            //console.log("state change");
+            console.log("state change");
                 return {
                     imgSrc: nextProps.imagein.imgSrc,
                     imgSrcLoaded: nextProps.imagein.isLoaded,
@@ -315,15 +341,16 @@ class ImgDropAndCrop extends Component {
         else return null;
     }
 
+
 //                                        src={this.state.newimageData64}
 //                                        src={imgSrc}
 
-
     render () {
         const {imgSrc,imgSrcLoaded} = this.state;
-        //console.log ("in imgDropandCrop render imgSrcLoaded=",imgSrcLoaded);
-        //console.log ("in imgDropandCrop render imgSrc=",imgSrc);
-        //console.log ("in imgDropandCrop render this.state.newimageData64=",this.state.newimageData64);
+        console.log ("in imgDropandCrop render imgSrcLoaded=",imgSrcLoaded);
+ //       console.log ("in imgDropandCrop render imgSrc=",imgSrc);
+//        console.log ("in imgDropandCrop render this.state.newimageData64=",this.state.newimageData64);
+
 
         if (!imgSrcLoaded) {
             return <p> Still Loading .... </p>
@@ -342,9 +369,7 @@ class ImgDropAndCrop extends Component {
                                         src={imgSrc} 
                                         crop={this.state.crop} 
                                         onImageLoaded={this.handleImageLoaded}
-                                        onComplete = {async (crop, percentCrop)=>{
-                                            await this.handleOnCropComplete(crop, percentCrop);
-                                            }}
+                                        onComplete = {this.handleOnCropComplete}
                                         onChange={this.handleOnCropChange}
                                     />
                                     <div  className={classes.CropBoxControls}>
