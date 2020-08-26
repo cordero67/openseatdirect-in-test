4ada2a6ab79129752c1439e31c5a2053985a5286
cordero67 <rafael.cordero@ymail.com>
2020-08-26 14:13:40 -0400

    paypal subscription code

--- src/Events/apiEvents.js
+++ src/Events/apiEvents.js
@@ -81,8 +81,8 @@ export const getAllEventData = () => {
     .then((response) => response.text())
     .then((result) => {
         let js = JSON.parse(result);
-        js.sort(compareValues("startDateTime", "asc"));
-        console.log("eventDescriptions ordered: ", js);
+        //js.sort(compareValues("startDateTime", "asc"));
+        //console.log("eventDescriptions ordered: ", js);
         return js;
     })
     .catch((error) => {
