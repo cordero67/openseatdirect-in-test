14880fb35ae0223e1101692a2d6bb2ec63fd1c4c
cordero67 <rafael.cordero@ymail.com>
2020-09-11 21:59:30 -0400

    removed src old

--- srcold/components/UI/Spinner/SpinnerNew.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import styles from "./SpinnerNew.module.css";
-
-const spinner = () => <div className={styles.Loader}>Loading...</div>;
-
-export default spinner;
--- srcold/config.js
+++
@@ -1,2 +0,0 @@
-export const API = process.env.REACT_APP_API_URL;
-export const PAYPAL_USE_SANDBOX = process.env.PAYPAL_USE_SANDBOX;
