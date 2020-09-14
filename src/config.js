5203dedf7ff7d046e949eb8cdf79caef5e240255
cordero67 <rafael.cordero@ymail.com>
2020-09-14 12:45:16 -0400

    env variables

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-export const PAYPAL_USE_SANDBOX=process.env.REACT_APP_PAYPAL_USE_SANDBOX;
-export const SUBSCRIPTION_PROMO_CODE=process.env.REACT_APP_SUBSCRIPTION_PROMO_CODE;
-export const API2=process.env.REACT_APP_API_URL2;
-export const API=process.env.REACT_APP_API_URL;
-export const nodeVar=process.env.NODE_ENV;
+export const API = process.env.REACT_APP_API_URL;
+export const PAYPAL_USE_SANDBOX = process.env.REACT_APP_PAYPAL_USE_SANDBOX;
+export const SUBSCRIPTION_PROMO_CODE = process.env.REACT_APP_SUBSCRIPTION_PROMO_CODE;
