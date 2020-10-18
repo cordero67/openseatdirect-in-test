b126647bb9a02933ea4acd5dfbf69d3e12158b17
cordero67 <rafael.cordero@ymail.com>
2020-10-18 13:56:30 -0400

    landing page edits

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -57,51 +57,109 @@ const Home = () => {
         })
   */
 
-  let marketingPoints = (
-    <Aux>
-      <div className={classes.UpperGrid}>
-        <div>Get Cash Now!</div>
-        <div>Absolutely NO Fees</div>
-        <div>Own All Your Data</div>
-      </div>
+    
+    let tagLine = () => {
+      if (screenSize >= 500) {
+        return (
+          <div style={{fontSize: "52px", lineHeight: "94px"}}>
+            GET CASH NOW!
+          </div>
+        ) 
+      } else {
+        return (
+          <div style={{paddingBottom: "20px", fontSize: "52px", lineHeight: "64px"}}>
+            GET CASH
+            <br></br>
+            NOW!
+          </div>
+        )
+      }
+    }
 
-      <div className={classes.MiddleGrid}>
-        <img
-            src={CashNow}
-            alt="OpenSeatDirect Logo"
-            style={{width: "100%"}}
-        />
-        <img
-            src={NoFees}
-            alt="OpenSeatDirect Logo"
-            style={{width: "100%"}}
-        />
-        <img
-            src={SingleLocation}
-            alt="OpenSeatDirect Logo"
-            style={{width: "100%"}}
-        />
-      </div>
-      
-      <div className={classes.LowerGrid}>
-          <div>Don't wait until after the event passes. Get paid on tickets you sell right away.</div>
-          <div>We're not kidding. You and your customers NEVER pay any ticketing fees.</div>
-          <div>All transaction information in one place: buyer emails, transaction info and data. </div>
-      </div>
-    </Aux>
-  )
+    let marketingLine = () => {
+      if (screenSize >= 1050) {
+        return (
+          <div style={{fontSize: "26px", lineHeight: "52px"}}>
+            Don't wait until after the event passes. Get paid on tickets you sell right away.
+          </div>
+        ) 
+      } else if (screenSize >= 900 && screenSize < 1050) {
+        return (
+          <div style={{fontSize: "22px", lineHeight: "48px"}}>
+            Don't wait until after the event passes. Get paid on tickets you sell right away.
+          </div>
+        )
+      } else if (screenSize >= 650 && screenSize < 900) {
+        return (
+          <div style={{fontSize: "26px", lineHeight: "52px"}}>
+            Don't wait until after the event passes.
+            <br></br>
+            Get paid on tickets you sell right away.
+          </div>
+        )
+      } else if (screenSize >= 450 && screenSize < 650) {
+        return (
+          <div style={{fontSize: "20px", lineHeight: "46px"}}>
+            Don't wait until after the event passes.
+            <br></br>
+            Get paid on tickets you sell right away.
+          </div>
+        )
+      } else {
+        return (
+          <div style={{fontSize: "20px", lineHeight: "46px"}}>
+            Don't wait until
+            <br></br>
+            after the event passes.
+            <br></br>
+            Get paid on tickets
+            <br></br>
+            you sell right away.
+          </div>
+        )
+      }
+    }
 
-    let tagLine = (
-      <div className={classes.MarketingText}>
-        GET CASH NOW!
-      </div>
-    )
-
-    let marketingLine = (
-      <div className={classes.DescriptionText}>
-        Don't wait until after the event passes. Get paid on tickets you sell right away.
-      </div>
-    )
+    let OSDimage = () => {
+      if (screenSize >= 1050) {
+        return (
+          <img
+            style={{maxHeight: "auto", maxWidth: "700px"}}
+            src={OSDImage}
+          >
+          </img>
+        )
+      } else if (screenSize >= 900 && screenSize < 1050) {
+        return (
+          <img
+            style={{maxHeight: "auto", maxWidth: "600px"}}
+            src={OSDImage}
+          >
+          </img>
+        )
+      } else if (screenSize >= 650 && screenSize < 900) {
+        return (
+          <img
+            style={{maxHeight: "auto", maxWidth: "500px"}}
+            src={OSDImage}
+          ></img>
+        )
+      } else if (screenSize >= 500 && screenSize < 650) {
+        return (
+          <img
+            style={{maxHeight: "auto", maxWidth: "400px"}}
+            src={OSDImage}
+          ></img>
+        )
+      } else {
+        return (
+          <img
+            style={{maxHeight: "auto", maxWidth: "300px"}}
+            src={OSDImage}
+          ></img>
+        )
+      }
+    }
 
     let marketingPhrase = () => {
       if (screenSize >= 1050) {
@@ -163,6 +221,223 @@ const Home = () => {
       }
     }
 
+    let MarketingPoints = () => {
+      if (screenSize >= 1050) {
+        return (
+          <div>
+            <div
+              className={classes.Grid}
+              style={{fontFamily: "Lato", paddingLeft: "calc(50% - 475px)", columnGap: "100px", fontSize: "26px", fontWeight: "600"}}>
+                <div>Get Cash Now!</div>
+                <div>Absolutely NO Fees</div>
+                <div>Own All Your Data</div>
+            </div>
+            <div
+              className={classes.ImageGrid}
+              style={{fontFamily: "Lato", paddingLeft: "calc(50% - 410px)", columnGap: "230px", fontSize: "16px"}}>
+  
+              <img
+                  src={CashNow}
+                  alt="OpenSeatDirect Logo"
+                  style={{width: "100%"}}
+              />
+              <img
+                  src={NoFees}
+                  alt="OpenSeatDirect Logo"
+                  style={{width: "100%"}}
+              />
+              <img
+                  src={SingleLocation}
+                  alt="OpenSeatDirect Logo"
+                  style={{width: "100%"}}
+              />
+            </div>
+            <div
+              className={classes.Grid}
+              style={{fontFamily: "Lato", paddingLeft: "calc(50% - 475px)", columnGap: "100px", fontSize: "16px"}}>
+                <div>
+                  Don't wait until after the
+                  <br></br>
+                  event passes. Get paid on
+                  <br></br>
+                  tickets you sell right away.
+                </div>
+                <div>
+                  We're not kidding.
+                  <br></br>
+                  You and your customers
+                  <br></br>
+                  NEVER pay any ticketing fees.
+                </div>
+                <div>
+                  All transaction information
+                  <br></br>
+                  in one place: buyer emails,
+                  <br></br>
+                  transaction info and data.
+                </div>
+            </div>
+          </div>
+        )
+      } else if (screenSize >= 900 && screenSize < 1050) {
+        return (
+          <div>
+            <div
+              className={classes.Grid}
+              style={{fontFamily: "Lato", paddingLeft: "calc(50% - 425px)", columnGap: "50px", fontSize: "26px", fontWeight: "600"}}>
+                <div>Get Cash Now!</div>
+                <div>Absolutely NO Fees</div>
+                <div>Own All Your Data</div>
+            </div>
+            <div
+              className={classes.ImageGrid}
+              style={{fontFamily: "Lato", paddingLeft: "calc(50% - 360px)", columnGap: "180px", fontSize: "16px"}}>
+  
+              <img
+                  src={CashNow}
+                  alt="OpenSeatDirect Logo"
+                  style={{width: "100%"}}
+              />
+              <img
+                  src={NoFees}
+                  alt="OpenSeatDirect Logo"
+                  style={{width: "100%"}}
+              />
+              <img
+                  src={SingleLocation}
+                  alt="OpenSeatDirect Logo"
+                  style={{width: "100%"}}
+              />
+            </div>
+            <div
+              className={classes.Grid}
+              style={{fontFamily: "Lato", paddingLeft: "calc(50% - 425px)", columnGap: "50px", fontSize: "16px"}}>
+                <div>
+                  Don't wait until after the
+                  <br></br>
+                  event passes. Get paid on
+                  <br></br>
+                  tickets you sell right away.
+                </div>
+                <div>
+                  We're not kidding.
+                  <br></br>
+                  You and your customers
+                  <br></br>
+                  NEVER pay any ticketing fees.
+                </div>
+                <div>
+                  All transaction information
+                  <br></br>
+                  in one place: buyer emails,
+                  <br></br>
+                  transaction info and data.
+                </div>
+            </div>
+          </div>
+        )
+      } else {
+        return (
+          <div>
+  
+            <div className={classes.DescriptionText} style={{fontSize: "26px", fontWeight: "600"}}>
+              <div>Get Cash Now!</div>
+            </div>
+            <div className={classes.ImageContainer}>
+              <img
+                style={{maxHeight: "auto", maxWidth: "200px"}}
+                src={CashNow}
+              >
+              </img>
+            </div>
+            <div style={{fontFamily: "Lato", textAlign: "center", fontSize: "20px", fontWeight: "400"}}>
+              Don't wait until after the
+              <br></br>
+              event passes. Get paid on
+              <br></br>
+              tickets you sell right away.
+            </div>
+            <br></br>
+            <br></br>
+  
+  
+            <div className={classes.DescriptionText} style={{fontSize: "26px", fontWeight: "600"}}>
+              <div>Absolutely NO Fees</div>
+            </div>
+            <div className={classes.ImageContainer}>
+              <img
+                style={{maxHeight: "auto", maxWidth: "200px"}}
+                src={NoFees}
+              >
+              </img>
+            </div>
+            <div style={{fontFamily: "Lato", textAlign: "center", fontSize: "20px", fontWeight: "400"}}>
+              We're not kidding.
+              <br></br>
+              You and your customers
+              <br></br>
+              NEVER pay any ticketing fees.
+            </div>
+            <br></br>
+            <br></br>
+  
+  
+            <div className={classes.DescriptionText} style={{fontSize: "26px", fontWeight: "600"}}>
+              <div>Own All Your Data</div>
+            </div>
+            <div className={classes.ImageContainer}>
+              <img
+                style={{maxHeight: "auto", maxWidth: "200px"}}
+                src={SingleLocation}
+              >
+              </img>
+            </div>
+            <div style={{fontFamily: "Lato", textAlign: "center", fontSize: "20px", fontWeight: "400"}}>
+              All transaction information
+              <br></br>
+                in one place: buyer emails,
+              <br></br>
+                transaction info and data.
+            </div>
+  
+  
+          </div>
+        )
+      }
+    }
+
+    let signUpText = () => {
+      if (screenSize >= 1050) {
+        return (
+          <div style={{fontSize: "26px", lineHeight: "52px"}}>
+            Sign up for a free trial or a subscription for as low as $7 a month.
+          </div>
+        ) 
+      } else if (screenSize >= 900 && screenSize < 1050) {
+        return (
+          <div style={{fontSize: "22px", lineHeight: "48px"}}>
+            Sign up for a free trial or a subscription for as low as $7 a month.
+          </div>
+        )
+      } else if (screenSize >= 650 && screenSize < 900) {
+        return (
+          <div style={{fontSize: "26px", lineHeight: "52px"}}>
+            Sign up for a free trial or a
+            <br></br>
+            subscription for as low as $7 a month.
+          </div>
+        )
+      } else {
+        return (
+          <div style={{fontSize: "20px", lineHeight: "46px"}}>
+            Sign up for a free trial or a
+            <br></br>
+            subscription for as low as $7 a month.
+          </div>
+        )
+      }
+    }
+
     const signUpForm = (
       <Aux>
         <div className="form-group">
@@ -216,22 +491,96 @@ const Home = () => {
         </div>
       </Aux>
     );
-      
-    let appointment = (
-      <div className={classes.DescriptionText}>
+
+    let appointment = () => {
+      if (screenSize >= 1050) {
+        return (
+          <div style={{fontSize: "26px", lineHeight: "52px"}}>
           Talk one-on-one with an onboarding specialist
-      </div>
-    )
+          </div>
+        ) 
+      } else if (screenSize >= 900 && screenSize < 1050) {
+        return (
+          <div style={{fontSize: "22px", lineHeight: "48px"}}>
+          Talk one-on-one with an onboarding specialist
+          </div>
+        )
+      } else if (screenSize >= 650 && screenSize < 900) {
+        return (
+          <div style={{fontSize: "26px", lineHeight: "52px"}}>
+            Talk one-on-one with 
+            <br></br>
+            an onboarding specialist
+          </div>
+        )
+      } else {
+        return (
+          <div style={{fontSize: "20px", lineHeight: "46px"}}>
+          Talk one-on-one with 
+          <br></br>
+          an onboarding specialist
+          </div>
+        )
+      }
+    }
 
     return (
       <div className={classes.MainContainer}>
+
         {isResizing ? null : (
           <div>
+            <div className={classes.TopContainer}>
+              <div className={classes.MarketingText}>
+                {tagLine()}
+              </div>
+              <div className={classes.MarketingText}>
+                {marketingLine()}
+              </div>
+            </div>
 
-            <div className={classes.TextContainer2}>
+            <div className={classes.OSDImageContainer}>
+              {OSDimage()}
+            </div>
+
+            <div className={classes.TextContainer}>
               <div className={classes.DescriptionText}>
                 {marketingPhrase()}
               </div>
+            </div>
+
+            <div className={classes.SectionContainer}>
+              {MarketingPoints()}
+            </div>
+
+            <div className={classes.SectionContainer}>
+              <div className={classes.DescriptionText}>
+                {signUpText()}
+              </div>
+              {screenSize >= 500 ? (
+                <div className={classes.SignUpForm}>
+                  <div>
+                    {signUpForm}
+                  </div>
+                </div>
+                ) :  (
+                  <div className={classes.SignUpFormTight}>
+                    <div>
+                      {signUpForm}
+                    </div>
+                  </div>
+                )
+              }
+            </div>
+
+
+            <div className={classes.SectionContainer}>
+              <div className={classes.DescriptionText}>
+                {appointment()}
+              </div>
+              <br></br>
+              <Button href="https://calendly.com/dahday/openseatdirect-connect?back=1&month=2020-10">
+                SCHEDULE APPOINTMENT
+              </Button>
             </div>
 
 
@@ -242,100 +591,3 @@ const Home = () => {
 }
 
 export default Home;
-/*
-
-          <div>
-            <div className={classes.TextContainer}>
-              {tagLine}
-              {marketingLine}
-            </div>
-
-            <div className={classes.ImageContainer}>
-              <img
-                className={classes.ImageBox}
-                src={OSDImage}
-              >
-              </img>
-            </div>
-
-            <div className={classes.TextContainer2}>
-              <div className={classes.DescriptionText}>
-                {marketingPhrase()}
-              </div>
-            </div>
-
-            <div className={classes.SectionContainer}>
-              {marketingPoints}
-            </div>
-
-            <div className={classes.SectionContainer}>
-              <div className={classes.DescriptionText}>
-                Sign up for a free trial or a subscription for as low as $7 a month.
-              </div>
-
-              <div className={classes.SignUpForm}>
-                <div>
-                  {signUpForm}
-                </div>
-              </div>
-            </div>
-
-            <div className={classes.SectionContainer}>
-              {appointment}
-              <br></br>
-              <Button href="https://calendly.com/dahday/openseatdirect-connect?back=1&month=2020-10">
-                SCHEDULE APPOINTMENT
-              </Button>
-            </div>
-
-          </div>
-
-*/
-
-
-
-
-/*
-: 
-      <div className={classes.MainContainer}>
-
-        <div className={classes.TextContainer}>
-          {marketingLine}
-        </div>
-
-        <div className={classes.ImageContainer}>
-          <img
-            className={classes.ImageBox}
-            src={OSDImage}
-          >
-          </img>
-        </div>
-
-        <div className={classes.TextContainer2}>
-          {marketingPhrase()}
-        </div>
-
-        <div className={classes.SectionContainer}>
-          {marketingPoints}
-        </div>
-        
-        <div className={classes.SectionContainer}>
-          <div className={classes.DescriptionText}>
-            Sign up for a free trial or a subscription for as low as $7 a month.
-          </div>
-
-          <div className={classes.SignUpForm}>
-            <div>
-              {signUpForm}
-            </div>
-          </div>
-        </div>
-
-        <div className={classes.SectionContainer}>
-          {appointment}
-          <br></br>
-          <Button href="https://calendly.com/dahday/openseatdirect-connect?back=1&month=2020-10">
-            SCHEDULE APPOINTMENT
-          </Button>
-        </div>
-      </div> }*/
