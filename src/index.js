c791ee5c3f0d9442a1c0d6487b35f6ce26065c87
mikem20 <mikem@openseatdirect.com>
2021-04-21 17:26:15 +0000

    removed Democarosel from index.js MM:RC

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import DemoCarousel from "./components/HomePage/DemoCarousel";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-//ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
