1d7f0f214e628f1ce949759ea8550d355a83147e
cordero67 <rafael.cordero@ymail.com>
2021-06-01 16:28:48 -0400

    hide ticket wallet

--- src/Users/Vendor/Components/VendorNavigation.js
+++ src/Users/Vendor/Components/VendorNavigation.js
@@ -6,62 +6,70 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
 import classes from "./VendorNavigation.module.css";
 
 const VendorNavigation = (props) => {
-  
   return (
     <Fragment>
-
       <div className={classes.DashboardTitle}>
-        <FontAwesomeIcon
-          color="white"
-          cursor="pointer"
-          icon={faHome}
-        />{" "}My Account
+        <FontAwesomeIcon color="white" cursor="pointer" icon={faHome} /> My
+        Account
       </div>
 
       <ul className={classes.NavigationBar}>
         <div className={classes.NavigationItems}>
-
           <li>
             <button
-              className={(
+              className={
                 props.pane === "events" ||
                 props.pane === "salesAnalytics" ||
                 props.pane === "ticketSales" ||
                 props.pane === "issueTickets" ||
                 props.pane === "editEvent"
-              ) ?
-                classes.NavigationButtonActive :
-                classes.NavigationButton
+                  ? classes.NavigationButtonActive
+                  : classes.NavigationButton
               }
               name="events"
-              onClick={props.clicked}>
+              onClick={props.clicked}
+            >
               My Events
             </button>
           </li>
 
           <li>
             <button
-              className={(props.pane === "create") ?
-                classes.NavigationButtonActive :
-                classes.NavigationButton
+              className={
+                props.pane === "create"
+                  ? classes.NavigationButtonActive
+                  : classes.NavigationButton
               }
               name="create"
-              onClick={props.clicked}>
+              onClick={props.clicked}
+            >
               Create Event
             </button>
           </li>
 
           <li>
             <button
-              className={(props.pane === "account") ?
-                classes.NavigationButtonActive :
-                classes.NavigationButton
+              className={
+                props.pane === "account"
+                  ? classes.NavigationButtonActive
+                  : classes.NavigationButton
               }
               name="account"
-              onClick={props.clicked}>
+              onClick={props.clicked}
+            >
               Account Settings
             </button>
           </li>
+        </div>
+      </ul>
+    </Fragment>
+  );
+};
+
+export default VendorNavigation;
+
+/*
+
 
           <hr className={classes.HorizontalDivider}/>
 
@@ -76,11 +84,4 @@ const VendorNavigation = (props) => {
               My Tickets
             </button>
           </li>
-
-        </div>
-      </ul>
-    </Fragment>
-  );
-};
-
-export default VendorNavigation;
+          */
