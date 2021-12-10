b20fd52cc78c040559f8e46e6b6f80b866e7ea74
cordero67 <rafael.cordero@ymail.com>
2021-12-10 10:31:44 -0500

    new ticket order

--- src/Events/TicketSelectionModal.js
+++ src/Events/TicketSelectionModal.js
@@ -3,30 +3,14 @@ import React, { useState, useEffect, Fragment } from "react";
 import Backdrop from "./Modals/Backdrop";
 import classes from "./TicketSelectionModal.module.css";
 
-let MainContainer = {};
-let MainContainer2 = {
-  fontFamily: "'Roboto', sans-serif",
-  position: "fixed",
-  zIndex: "700",
-  backgroundColor: `white`,
-  border: "2px solid #ccc",
-  height: "700px",
-  //top: `calc((${inHeight}px - 720px) / 2)`,
-  //left: `calc((${inWidth}px - 1080px) / 2)`,
-  top: "calc((100vh - 720px) / 2)",
-  left: "calc((100vw - 1080px) / 2)",
-  //paddingLeft: `25px`,
-  //paddingRight: `25px`,
-  //paddingBottom: `calc((${inHeight}px - 720px) / 2)`,
-  boxSizing: "border-box",
-  transition: "all 0.3s ease-out",
-};
-
 const TicketSelectionModal = (props) => {
   const [isRestyling, setIsRestyling] = useState(false); // defines styling variables
   const [display, setDisplay] = useState("main"); // defines panel displayed: main, registration, spinner, confirmation, connection
   const [showDoublePane, setShowDoublePane] = useState(false); // defines single or double panel display on main page
   const [showOrderSummaryOnly, setShowOrderSummaryOnly] = useState(false); // defines panel display for a single panel display on main page
+  const [mainContainer, setMainContainer] = useState({});
+  const [mainGrid, setMainGrid] = useState({});
+  const [eventTicketSection, setEventTicketSection] = useState({});
 
   useEffect(() => {
     //eventData(queryString.parse(window.location.search).eventID);
@@ -45,25 +29,31 @@ const TicketSelectionModal = (props) => {
       //setShowDoublePane(true);
     }
     // sets styling parameters
-    MainContainer = MainContainerStyling(inWidth, inHeight);
+    //MainContainer = MainContainerStyling(inWidth, inHeight);
+    setMainContainer(MainContainerStyling(inWidth, inHeight));
     //MainGrid = MainGridStyling(inWidth, inHeight);
+    setMainGrid(MainGridStyling(inWidth, inHeight));
     //EventTicketSection = EventTicketSectionStyling(inWidth, inHeight);
+    setEventTicketSection(EventTicketSectionStyling(inWidth, inHeight));
     //OrderSummarySection = OrderSummarySectionStyling(inWidth, inHeight);
     //OrderSummarySectionAlt = OrderSummarySectionAltStyling(inWidth, inHeight);
     setIsRestyling(false);
   };
 
   const MainContainerStyling = (inWidth, inHeight) => {
+    let mainContainer = {};
     if (inWidth < 660) {
       console.log("width < 660");
-      MainContainer = {
+      mainContainer = {
         fontFamily: "'Roboto', sans-serif",
         position: "fixed",
         zIndex: "700",
         backgroundColor: `white`,
         //border: "2px solid #ccc",
-        border: "2px solid red",
+        //border: "2px solid purple",
         height: `${inHeight}px`,
+        top: "0px",
+        left: "0px",
         //paddingTop: `0px`,
         //paddingLeft: `0px`,
         //paddingRight: `0px`,
@@ -71,16 +61,17 @@ const TicketSelectionModal = (props) => {
         boxSizing: "border-box",
         transition: "all 0.3s ease-out",
       };
-    } else if (inHeight < 700) {
-      console.log("height < 700");
-      MainContainer = {
+    } else if (inHeight < 720) {
+      console.log("height < 720");
+      mainContainer = {
         fontFamily: "'Roboto', sans-serif",
         position: "fixed",
         zIndex: "700",
         backgroundColor: `white`,
         //border: "2px solid #ccc",
-        border: "2px solid red",
+        //border: "2px solid green",
         height: `${inHeight}px`,
+        top: "0px",
         left: `calc((${inWidth}px - 1080px) / 2)`,
         //paddingTop: `0px`,
         //paddingLeft: `25px`,
@@ -90,15 +81,15 @@ const TicketSelectionModal = (props) => {
         transition: "all 0.3s ease-out",
       };
     } else {
-      console.log("width > 700");
-      MainContainer = {
+      console.log("width > 720");
+      mainContainer = {
         fontFamily: "'Roboto', sans-serif",
         position: "fixed",
         zIndex: "700",
         backgroundColor: `white`,
         //border: "2px solid #ccc",
-        border: "2px solid red",
-        height: "700px",
+        //border: "2px solid red",
+        height: "720px",
         top: `calc((${inHeight}px - 720px) / 2)`,
         left: `calc((${inWidth}px - 1080px) / 2)`,
         //paddingLeft: `25px`,
@@ -108,6 +99,8 @@ const TicketSelectionModal = (props) => {
         transition: "all 0.3s ease-out",
       };
     }
+
+    return mainContainer;
 
     /*
       .MainContainer {
@@ -124,24 +117,207 @@ const TicketSelectionModal = (props) => {
         //transition: all 0.3s ease-out;
       }
     */
-
-    return MainContainer;
   };
 
+  const MainGridStyling = (inWidth, inHeight) => {
+    let mainGrid = {};
+    if (inWidth < 660) {
+      // width < 660px, height does not matter
+      mainGrid = {
+        backgroundColor: `#fff`,
+        margin: `auto`,
+        height: `${inHeight}px`,
+        display: `grid`,
+        gridTemplateColumns: `auto`,
+      };
+    } else if (inWidth < 790) {
+      if (inHeight < 720) {
+        // height < 720px
+        mainGrid = {
+          backgroundColor: `#fff`,
+          margin: `auto`,
+          height: `${inHeight}px`,
+          display: `grid`,
+          gridTemplateColumns: `auto`,
+        };
+      } else {
+        // height >= 720px
+        mainGrid = {
+          backgroundColor: `#fff`,
+          margin: `auto`,
+          height: `720px`,
+          display: `grid`,
+          gridTemplateColumns: `auto`,
+        };
+      }
+    } else if (inWidth < 960) {
+      // width < 960px, NEED TO CHECK HEIGHT
+      if (inHeight < 720) {
+        // height < 720px
+        mainGrid = {
+          backgroundColor: `#fff`,
+          margin: `auto`,
+          height: `${inHeight}px`,
+          display: `grid`,
+          gridTemplateColumns: `auto 320px`,
+        };
+      } else {
+        // height >= 720px
+        mainGrid = {
+          backgroundColor: `#fff`,
+          margin: `auto`,
+          height: `720px`,
+          display: `grid`,
+          gridTemplateColumns: `auto 320px`,
+        };
+      }
+    } else if (inWidth < 1140) {
+      if (inHeight < 720) {
+        // height < 720px
+        mainGrid = {
+          backgroundColor: `#fff`,
+          margin: `auto`,
+          height: `${inHeight}px`,
+          display: `grid`,
+          gridTemplateColumns: `auto 360px`,
+        };
+      } else {
+        // height >= 720px
+        mainGrid = {
+          backgroundColor: `#fff`,
+          margin: `auto`,
+          height: `720px`,
+          display: `grid`,
+          gridTemplateColumns: `auto 360px`,
+        };
+      }
+    } else {
+      if (inHeight < 720) {
+        console.log("inHeight: ", inHeight);
+        // height < 720px
+        mainGrid = {
+          backgroundColor: `#fff`,
+          margin: `auto`,
+          border: "2px solid blue",
+          height: `${inHeight}px`,
+          width: `1080px`,
+          display: `grid`,
+          gridTemplateColumns: `720px 360px`,
+        };
+      } else {
+        // height >= 720px
+        mainGrid = {
+          backgroundColor: `#fff`,
+          margin: `auto`,
+          border: "2px solid orange",
+          height: `720px`,
+          width: `1080px`,
+          display: `grid`,
+          gridTemplateColumns: `720px 360px`,
+        };
+      }
+    }
+    return mainGrid;
+  };
+
+  const EventTicketSectionStyling = (inWidth, inHeight) => {
+    let eventTicketSection = {};
+
+    if (inWidth < 480) {
+      // width < 480px, height does not matter
+      eventTicketSection = {
+        backgroundColor: `#fff`,
+        height: `calc(${inHeight}px - 140px)`,
+        paddingTop: `30px`,
+        paddingLeft: `10px`,
+        paddingRight: `10px`,
+        textAlign: `left`,
+        scrollbarWidth: `10px`,
+        overflowY: `auto`,
+      };
+    } else if (inWidth < 660) {
+      // width < 660px, height does not matter
+      eventTicketSection = {
+        backgroundColor: `#fff`,
+        height: `calc(${inHeight}px - 140px)`,
+        paddingTop: `30px`,
+        paddingLeft: `25px`,
+        paddingRight: `25px`,
+        textAlign: `left`,
+        scrollbarWidth: `10px`,
+        overflowY: `auto`,
+      };
+    } else {
+      // width >= 1140px, NEED TO CHECK HEIGHT
+      if (inHeight < 720) {
+        // height < 720px
+        eventTicketSection = {
+          backgroundColor: `#fff`,
+          height: `calc(${inHeight}px - 140px)`,
+          paddingTop: `30px`,
+          paddingLeft: `25px`,
+          paddingRight: `25px`,
+          textAlign: `left`,
+          scrollbarWidth: `10px`,
+          overflowY: `auto`,
+        };
+      } else {
+        // height >= 720px
+        eventTicketSection = {
+          backgroundColor: `#fff`,
+          height: `580px`,
+          paddingTop: `30px`,
+          paddingLeft: `25px`,
+          paddingRight: `25px`,
+          textAlign: `left`,
+          scrollbarWidth: `10px`,
+          overflowY: `auto`,
+        };
+      }
+    }
+
+    return eventTicketSection;
+  };
+
+  const ticketPaneOLD = () => {
+    return <div>ticket paaaaaaaaaaaaaaaaaaaaaaaane</div>;
+  };
+
+  // creates ticket pane with promo form and ticket sections
   const ticketPane = () => {
-    return <div>ticket pane</div>;
-  };
-  const orderPane = () => {
-    return <div>order pane</div>;
+    return (
+      <div>Ticket Pane Section</div>
+      /*
+      <div className={classes.MainItemLeft}>
+        <div className={classes.EventHeader}>{eventHeader()}</div>
+        <div style={EventTicketSection}>
+          {promoOption()}
+          {ticketItems()}
+          <div className={classes.EventDescription}>
+            Powered by{" "}
+            <NavLink to="/" exact>
+              <img
+                className={classes.ImageBox}
+                src={OSDLogo}
+                alt="OpenSeatDirect Logo"
+              />
+            </NavLink>
+          </div>
+        </div>
+        <div className={classes.EventFooter}>
+          <div className={classes.CartLink}>{cartLink(showDoublePane)}</div>
+          <div className={classes.TotalAmount}>
+            {totalAmount(showDoublePane)}
+          </div>
+          <div style={{ textAlign: "right" }}>{checkoutButton()}</div>
+        </div>
+      </div>
+      */
+    );
   };
 
-  const MainGrid = {
-    backgroundColor: `#fff`,
-    margin: `auto`,
-    height: `720px`,
-    width: `1080px`,
-    display: `grid`,
-    gridTemplateColumns: `720px 360px`,
+  const orderPane = () => {
+    return <div>order paaaaaaaaaaaaaaaaaaaaaaaaane</div>;
   };
 
   // LOOKS GOOD
@@ -151,16 +327,16 @@ const TicketSelectionModal = (props) => {
       //if (showDoublePane) {
       if (true) {
         return (
-          <div style={MainGrid}>
+          <div style={mainGrid}>
             {ticketPane()}
             {orderPane()}
           </div>
         );
         //} else if (!showOrderSummaryOnly) {
       } else if (true) {
-        return <div style={MainGrid}>{ticketPane()}</div>;
+        return <div style={mainGrid}>{ticketPane()}</div>;
       } else {
-        return <div style={MainGrid}>{orderPane()}</div>;
+        return <div style={mainGrid}>{orderPane()}</div>;
       }
     } else return null;
   };
@@ -173,7 +349,7 @@ const TicketSelectionModal = (props) => {
           transform: true ? "translateY(0)" : "translateY(-100vh)",
           opacity: true ? "1" : "0",
         }}
-        style={MainContainer}
+        style={mainContainer}
       >
         {mainDisplay()}
       </div>
