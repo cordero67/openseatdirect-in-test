400a0dca7b8c59032ab06fa1e0e6e934b6ea21a5
cordero67 <rafael.cordero@ymail.com>
2022-05-23 09:30:13 -0400

    fixes

--- src/Users/Vendor/Modals/OpennodeModal.js
+++ src/Users/Vendor/Modals/OpennodeModal.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, Fragment } from "react";
-import OpenNodeDisplay from "../../../Authentication/Components/OpenNodeDisplay";
 import ErrorDisplay from "../../../Authentication/Components/ErrorDisplay";
 
 import Backdrop from "./Backdrop";
